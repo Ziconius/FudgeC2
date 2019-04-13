f7c1a81fa759e567a216b7c8ea99f336634bf20e
Kris Anderson <kristoferanderson90@gmail.com>
2019-04-13 17:02:40 +0100

    Minor changes to UserManagement

--- ServerApp/modules/UserManagement.py
+++ ServerApp/modules/UserManagement.py
@@ -3,17 +3,9 @@ import random
 import string
 class UserManagementController():
     db = Database()
-    # def __init__(self, DatabaseConnector):
-    #     print(type(DatabaseConnector))
-    #     if str(type(DatabaseConnector)) == "<class 'Database.Database'>":
-    #         self.db = DatabaseConnector
-    #         return
-    #     else:
-    #         raise ValueError("Not a valid container")
 
     def AddUser(self, formdata=None, submitting_user=None):
         # -- Refacteror/Clean Add failure checks
-        print("Form: ",formdata,"\nUser: ",submitting_user)
         # -- Check for the keys in formdata, if none then return an error.
         # -- UserName/is_admin
 
@@ -27,9 +19,7 @@ class UserManagementController():
             Result_Dict['reason'] = "Username too short"
             return Result_Dict
         U = self.db.Get_UserObject(submitting_user)
-        print(U.admin)
         if U.admin:
-            print("blah: ",formdata['UserName'])
             G = self.db.Get_UserObject(formdata['UserName'])
             admin = False
             if 'is_admin' in formdata:
@@ -37,14 +27,12 @@ class UserManagementController():
             if G == None:
                 N=8
                 pw=''.join(random.choices(string.ascii_uppercase + string.digits, k=N))
-                print("::",pw)
                 self.db.Add_User(formdata['UserName'],pw,admin)
                 Result_Dict['result']=True
                 Result_Dict['reason']=str(formdata['UserName']+" now created. Password is: "+pw+" <br> Take note of this, it will not be visable again.")
             else:
                 Result_Dict['result'] = False
                 Result_Dict['reason'] = "User already exists."
-            print("Admin")
             # -- Validate
 
         return Result_Dict
@@ -62,7 +50,6 @@ class UserManagementController():
         # Improve variable names
         # --
         if len(Users) < 1:
-            print("too few")
             return False
         if Users:
             for User in Users:
