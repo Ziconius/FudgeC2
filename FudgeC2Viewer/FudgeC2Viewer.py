2bd7bdf49572fd2d81cd7d2252f6ef9db45db453
Kris Anderson <kristoferanderson90@gmail.com>
2019-09-21 10:10:51 +0100

    Adding place holder for the Campaign Viewer tool.

---
+++ FudgeC2Viewer/FudgeC2Viewer.py
@@ -0,0 +1,16 @@
+# Upload,
+# display
+
+
+# page for uploading/main page - Upload endpoint
+# return view page
+#   view page reject if no upload
+#   display and filter campaign based on what user wants.
+
+
+# functions
+#   database
+#   decrytpt
+#   load database
+#   how to understand Schema?
+#   dump to viewer page.
